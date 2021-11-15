9d282026007e845830f807b246fee2912bd26c4c
leo <cedillo47@gmail.com>
2021-11-15 15:16:12 +0000

    compleate

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //write your code here
+
+const findLongestWord = array =>{
+  let maxword = "";
+  for(let word of array){
+    if(maxword.length < word.length){
+      maxword = word
+     }
+    } 
+    // for(let word of array){
+    // if(maxword.length <= word.length){
+    //   maxword = maxword
+    //  }
+    // } 
+    
+    return maxword
+  }
+
+
+console.log(findLongestWord(["helll4","helll8","whats","up"]))
